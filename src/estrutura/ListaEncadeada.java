d3e189ce41c4bffa520b7b200883329f2ddfe18c
wiliamtrancoso <wileleh@gmail.com>
2016-11-20 23:51:38 -0200

    lista encadeada

---
+++ src/estrutura/ListaEncadeada.java
@@ -0,0 +1,19 @@
+package estrutura;
+
+import modelo.No;
+
+public class ListaEncadeada {
+	private No primeiro;
+	private No ultimo;
+	private int quantidadeNos = 0;
+	
+	public void add(No no){
+		if(quantidadeNos == 0){
+			primeiro = no;
+			ultimo = no;
+			quantidadeNos++;
+		}else{
+		}
+	}
+	
+}
