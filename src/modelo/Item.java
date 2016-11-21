413a923902069336c6eef71e41edfe3bbb9e136f
DESKTOP-724K63Q\keoma <keoma.klein1@gmail.com>
2016-11-20 23:34:39 -0200

    Alteração em item a implementacao de metodo de leitura e tratamento de arquivo em ArquivoUtilitario

--- src/modelo/Item.java
+++ src/modelo/Item.java
@@ -1,6 +1,38 @@
 package modelo;
 
 public class Item {
-	private String palavra;
 	private int paragrafo;
+	private String palavra;
+	private Item proximo;
+	
+	public Item(int paragrafo, String palavra) {
+		super();
+		this.paragrafo = paragrafo;
+		this.palavra = palavra;
+	}
+
+	public int getParagrafo() {
+		return paragrafo;
+	}
+
+	public void setParagrafo(int paragrafo) {
+		this.paragrafo = paragrafo;
+	}
+
+	public String getPalavra() {
+		return palavra;
+	}
+
+	public void setPalavra(String palavra) {
+		this.palavra = palavra;
+	}
+	public Item getProximo() {
+		return this.proximo;
+	}
+
+	public void setProximo(Item item) {
+		this.proximo = item;
+	}
+	
+	
 }
--- src/utilitario/ArquivoUtilitario.java
+++ src/utilitario/ArquivoUtilitario.java
@@ -1,5 +1,95 @@
 package utilitario;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import modelo.Item;
+import modelo.No;
+
 public abstract class ArquivoUtilitario {
+	
+	
+	private static Item[] arquivoToVetorItem(String caminho){
+
+		Lista lista = new Lista();
+		
+		try {
+			FileReader fr = new FileReader(new File(caminho));
+			BufferedReader bf = new BufferedReader(fr);
+			String linha = bf.readLine();
+			int numLinha = 0;
+			String separador = "#";
+			
+			while(linha != null){
+
+				String[] dado = verificaTexto(linha, separador).split(separador);
+				
+				for (int i = 0; i < dado.length; i++) {
+					lista.inserirUltimo(new No(new Item(numLinha, dado[i] + (i==dado.length-1?"\n\n":""))));
+				}
+				
+				numLinha++;
+				linha = bf.readLine();
+			}
+		
+		fr.close();	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Item atual = lista.getPrimeiro().getInfo();
+		Item[] itens = new Item[lista.getQuantNos()];
+		
+		int ini = 0;
+		while(atual != null){
+		  itens[ini] = atual;
+		  atual = atual.getProximo();
+		  ini++;
+		}
+		return itens;
+	}
+	
+	
+	/**
+	 * 
+	 * 
+	 * */
+	public static String verificaTexto(String a, String separador){
+		Lista lista = new Lista();
+		String[] temp = a.replace(","," ").replace("."," ").split("|");
+		StringBuilder palavraTemp = new StringBuilder();
+		boolean eEspaco = false;
+		int cont = 0;
+		
+		for (int p = 0; p < temp.length; p++) {
+	
+			if(temp[p].equals(" ")){
+				eEspaco = true;
+				
+			}else{
+				palavraTemp.append(temp[p]);
+				eEspaco = false;
+			}
+
+			if(!palavraTemp.toString().equals("") && (eEspaco || (temp.length-1 == p))){
+				lista.inserirUltimo(new No(new Item(cont, palavraTemp.toString())));
+				cont++;
+				palavraTemp = new StringBuilder();
+				eEspaco = false;
+			}
+			
+		}
+		
+		Item atual = lista.getPrimeiro().getInfo();
+		palavraTemp = new StringBuilder();
+		
+		while(atual != null){
+			palavraTemp.append(atual.getPalavra());
+			palavraTemp.append(separador);
+			atual = atual.getProximo();
+		}
+		return palavraTemp.toString();
+	}
 
 }
