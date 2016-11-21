3a90ebf11cddc2a1948af8479c48f684ff044130
DESKTOP-724K63Q\keoma <keoma.klein1@gmail.com>
2016-11-20 23:59:23 -0200

    Add get set em ListaEncadeada e correções em ArquivoUtilitario

--- src/estrutura/ListaEncadeada.java
+++ src/estrutura/ListaEncadeada.java
@@ -15,5 +15,31 @@ public class ListaEncadeada {
 		}else{
 		}
 	}
+
+	public No getPrimeiro() {
+		return primeiro;
+	}
+
+	public void setPrimeiro(No primeiro) {
+		this.primeiro = primeiro;
+	}
+
+	public No getUltimo() {
+		return ultimo;
+	}
+
+	public void setUltimo(No ultimo) {
+		this.ultimo = ultimo;
+	}
+
+	public int getQuantidadeNos() {
+		return quantidadeNos;
+	}
+
+	public void setQuantidadeNos(int quantidadeNos) {
+		this.quantidadeNos = quantidadeNos;
+	}
+	
+	
 	
 }
