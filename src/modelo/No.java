13bd94d3e344f979d2597a8879d9d5d78a47245c
DESKTOP-724K63Q\keoma <keoma.klein1@gmail.com>
2016-11-20 23:24:54 -0200

    Get e Set na classe No

--- src/modelo/No.java
+++ src/modelo/No.java
@@ -3,4 +3,24 @@ package modelo;
 public class No {
 	private Item item;
 	private No proximoNo;
+	
+	public No(Item item) {
+		this.item = item;
+	}
+
+	public No getProx() {
+		return this.proximoNo;
+	}
+
+	public void setProx(No novo) {
+		this.proximoNo = novo;
+	}
+
+	public Item getInfo() {
+		return item;
+	}
+
+	public void setInfo(Item item) {
+		this.item = item;
+	}
 }
