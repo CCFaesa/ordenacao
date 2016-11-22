a31a9e57d97dc21a3d1d36fbd1806873561245d2
DESKTOP-724K63Q\keoma <keoma.klein1@gmail.com>
2016-11-22 20:29:04 -0200

    Up!

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -3,17 +3,28 @@ package principal;
 import modelo.Item;
 import ordenador.ABB;
 import ordenador.AVL;
+import utilitario.ArquivoUtilitario;
 
 public class Principal {
 	public static void main(String[] args) {
-//		Item[] a = ArquivoUtilitario.arquivoToVetorItem("Texto1.txt");
-//	
-//		for (Item item : a) {
-//			System.out.println(item.getPalavra());
-//		}
-//		System.out.println("Ola ordenacao");
 		
-		AVL arvore = new AVL();
+		
+	
+	
+	}
+
+	private void testeListaEncadeada(){
+		Item[] a = ArquivoUtilitario.arquivoToVetorItem("Texto1.txt");
+	
+		for (Item item : a) {
+			System.out.println(item.getPalavra());
+		}
+		System.out.println("Ola ordenacao");
+
+	}
+	
+	private void testeAbb(){
+		ABB arvore = new ABB();
 		arvore.insere(new Item(1, "Adedonha"));
 		arvore.insere(new Item(1, "Adedo"));
 		arvore.insere(new Item(2, "Boneca"));
@@ -23,7 +34,6 @@ public class Principal {
 		arvore.insere(new Item(2, "Sapo"));
 		arvore.insere(new Item(2, "Rato"));
 		System.out.println();
-		
 	
 	}
 }
