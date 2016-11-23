1d542febc598c0571bfc1ad81648d536e2f62517
Wiliam Trancoso <wtrancoso@qualityautomacao.com>
2016-11-23 13:33:56 -0200

    generalizacao do no da arvore

---
+++ src/modelo/NoArvore.java
@@ -0,0 +1,47 @@
+package modelo;
+
+import estrutura.ListaEncadeada;
+
+public class NoArvore {
+	private NoArvore noEsquerda;
+	private NoArvore noDireita;
+	private ListaEncadeada lista;
+	private int fatorBalanceamento;
+	
+	public NoArvore(Item item){
+		lista = new ListaEncadeada();
+		lista.add(item);
+	}
+	
+	public NoArvore getNoEsquerda() {
+		return noEsquerda;
+	}
+
+	public void setNoEsquerda(NoArvore no) {
+		this.noEsquerda = no;
+	}
+
+	public NoArvore getNoDireita() {
+		return noDireita;
+	}
+
+	public void setNoDireita(NoArvore no) {
+		this.noDireita = no;
+	}
+	
+	public No getPrimeiroNo(){
+		return lista.getPrimeiro();
+	}
+
+	public int getFatorBalanceamento() {
+		return fatorBalanceamento;
+	}
+
+	public void setFatorBalanceamento(int fatorBalanceamento) {
+		this.fatorBalanceamento = fatorBalanceamento;
+	}
+
+	public void add(Item item){
+		lista.add(item);
+	}
+}
